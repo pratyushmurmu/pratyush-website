10a651800253764dd7bc4110f7144f52c3265a08
pratyush <pratyushmurmu2004@gmail.com>
2026-01-20 20:24:38 +0530

    Worked on it's interactivity and UX like smooth scroll, transitions

--- bts.js
+++ bts.js
@@ -4,3 +4,37 @@ const navMenu = document.querySelector('nav ul');
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
+
+const navLinks= document.querySelectorAll("nav ul li a")
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e){
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href')
+    const targetSection = document.querySelector(targetId);
+
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+})
+
+
+const revealSections = document.querySelectorAll('.reveal');
+
+function revealOnScroll(){
+  revealSections.forEach(section => {
+    const windowHeight = window.innerHeight;
+    const revealTop = section.getBoundingClientRect().top;
+    const revealPoint = 150;
+  
+
+  if(revealTop < windowHeight - revealPoint){
+    section.classList.add('active');
+ }
+});
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
+
