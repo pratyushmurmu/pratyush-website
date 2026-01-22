43d5162578c3508ca7d9aafc8aab625db4a4615d
pratyush <pratyushmurmu2004@gmail.com>
2026-01-23 01:03:19 +0530

    Added forms in Contact section and dark/light mode toggle.

--- bts.js
+++ bts.js
@@ -38,3 +38,20 @@ function revealOnScroll(){
 window.addEventListener('scroll', revealOnScroll);
 window.addEventListener('load', revealOnScroll);
 
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
+
+// Load saved theme
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "light") {
+  body.classList.add("light");
+  toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("light");
+
+  const isLight = body.classList.contains("light");
+  toggleBtn.textContent = isLight ? "☀️" : "🌙";
+  localStorage.setItem("theme", isLight ? "light" : "dark");
+});
